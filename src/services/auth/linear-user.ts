133471a6d421655fbaf5acd41dedc7cf1ffdd056
Jens Astrup <jensaiden@gmail.com>
2024-07-16 16:58:23 -0400

    Create getOrCreateUser service for authentication
    A new service function named getOrCreateUser has been added for user authentication. This function, used within the authorization process, fetches the user from Linear, and encrypts the user's access token, after which the user is upserted in the database. In the authorization service, instead of directly using the access token, getUserOrCreate is invoked to obtain the user's encrypted token.

---
+++ src/services/auth/linear-user.ts
@@ -0,0 +1,10 @@
+import { LinearClient, User } from '@linear/sdk'
+
+
+
+async function getLinearUser(accessToken: string): Promise<User> {
+  const linearClient = new LinearClient({ accessToken })
+  return await linearClient.viewer
+}
+
+export default getLinearUser

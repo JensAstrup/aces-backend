c7149fab97f67911f74446dd6fa9acf080e7852e
Jens Astrup <jensaiden@gmail.com>
2024-07-17 13:29:19 -0400

    Remove console log statement

--- src/services/views/get-favorite-views.ts
+++ src/services/views/get-favorite-views.ts
@@ -6,7 +6,6 @@ import decrypt from '@aces/util/encryption/decrypt'
 
 async function getFavoriteViews(user: User): Promise<CustomView[]> {
   const accessToken = decrypt(user.token)
-  console.log(accessToken)
   const linear = new LinearClient({ accessToken })
   // Using linear SDK, retrieve a user's favorite views
   const favorites = await linear.customViews()

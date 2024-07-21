6b5cd12ef4943d3e82d424347bbe05e55c87bb14
Jens Astrup <jensaiden@gmail.com>
2024-07-21 13:57:06 -0400

    Remove unnecessary comment

--- src/services/views/get-favorite-views.ts
+++ src/services/views/get-favorite-views.ts
@@ -7,7 +7,6 @@ import decrypt from '@aces/util/encryption/decrypt'
 async function getFavoriteViews(user: User): Promise<Favorite[]> {
   const accessToken = decrypt(user.token)
   const linear = new LinearClient({ accessToken })
-  // Using linear SDK, retrieve a user's favorite views
   const favorites = await linear.favorites()
   return favorites.nodes.filter(favorite => favorite.type === 'customView')
 }

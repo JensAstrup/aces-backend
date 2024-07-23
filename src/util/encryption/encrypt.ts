bb9fb50f0b243c8eaf7cda71ad852ef84b199660
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-07-23 19:14:39 -0400

    ACEB-19: Improve encryption and decryption (#34)
    * Improve encryption and decryption
    
    * Lint removal
    
    * Update tests, improve key configuration check
    
    * Lint removal

--- src/util/encryption/encrypt.ts
+++ src/util/encryption/encrypt.ts
@@ -1,16 +1,27 @@
-import { AES } from 'crypto-js'
+import { createCipheriv, randomBytes, scryptSync } from 'crypto'
 
 import ConfigurationError from '@aces/errors/configuration-error'
 
 
-const KEY = process.env.ENCRYPTION_KEY
 
-if (KEY === undefined) {
-  throw new ConfigurationError('ENCRYPTION_KEY is required')
-}
 
 function encrypt(data: string): string {
-  return AES.encrypt(data, KEY!).toString()
+  const KEY = process.env.ENCRYPTION_KEY
+  const IV_LENGTH = 16
+
+  if (KEY === undefined) {
+    throw new ConfigurationError('ENCRYPTION_KEY is required')
+  }
+
+  // Derive a key using scrypt with a salt
+  const KEY_LENGTH = 32
+  const key = scryptSync(KEY, 'salt', KEY_LENGTH)
+
+  const iv = randomBytes(IV_LENGTH)
+  const cipher = createCipheriv('aes-256-cbc', key, iv)
+  let encrypted = cipher.update(data, 'utf8', 'hex')
+  encrypted += cipher.final('hex')
+  return `${iv.toString('hex')}:${encrypted}`
 }
 
 export default encrypt

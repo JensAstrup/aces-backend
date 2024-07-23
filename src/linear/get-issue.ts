989d833e0e136006dd6d975a821e15c6ff446436
Jens Astrup <jensaiden@gmail.com>
2024-07-22 23:45:37 -0400

    Add ability to fetch singular issue from Linear

---
+++ src/linear/get-issue.ts
@@ -0,0 +1,23 @@
+import { Issue, LinearClient } from '@linear/sdk'
+
+import { issueFields } from '@aces/linear/issue-query'
+
+
+interface IssueResponse {
+    issue: Issue | null
+}
+
+async function getIssue(issueId: string, accessToken: string): Promise<Issue | null> {
+  const client = new LinearClient({ accessToken })
+  const graphQlClient = client.client
+  const query = `query issue($issueId: String!) {
+        issue(id: $issueId) {
+        ${issueFields}
+        }
+    }`
+  const variables = { issueId }
+  const response: IssueResponse = await graphQlClient.request(query, variables)
+  return response.issue ? response.issue : null
+}
+
+export default getIssue

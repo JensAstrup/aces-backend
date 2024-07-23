8056230c9f13dc1af404ee4ee0b28d7b5ba249a5
Jens Astrup <jensaiden@gmail.com>
2024-07-23 01:18:15 -0400

    remove unnecessary console log statement

--- src/middleware/set-user.ts
+++ src/middleware/set-user.ts
@@ -4,7 +4,6 @@ import getOrCreateUser from '@aces/services/auth/get-or-create-user'
 
 
 async function setUser(request: Request, response: Response, next: NextFunction): Promise<void> {
-  console.log('setUser')
   const token = request.headers.authorization
   if (token) {
     request.user = await getOrCreateUser(token, true)

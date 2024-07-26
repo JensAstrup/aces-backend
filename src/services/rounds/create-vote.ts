6dfce79fa31baee4f54cc335fd58463e9d71545c
Jens Astrup <jensaiden@gmail.com>
2024-07-25 22:54:28 -0400

    Overwrite existing votes
    Only allow one vote per issue on a round by user

--- src/services/rounds/create-vote.ts
+++ src/services/rounds/create-vote.ts
@@ -4,10 +4,19 @@ import { Issue, PrismaClient, User, Vote } from '@prisma/client'
 const prismaClient = new PrismaClient()
 
 async function createVote(issue: Issue, user: User, estimate: number): Promise<Vote> {
-  const vote = await prismaClient.vote.create({
-    data: {
+  const vote = await prismaClient.vote.upsert({
+    where: {
+      issueId_userId: {
+        issueId: issue.id,
+        userId: user.id
+      }
+    },
+    create: {
       issueId: issue.id,
       userId: user.id,
+      vote: estimate
+    },
+    update: {
       vote: estimate
     }
   })

b856ea083b9cf194b1c6e5c193af064f0493476b
Jens Astrup <jensaiden@gmail.com>
2024-07-25 22:57:32 -0400

    Refactor access token handling in user authentication
    Separate token decryption and Linear user retrieval into a new function `getAndUpdateLinearUser`. Add logic to handle anonymous access tokens directly in `getOrCreateUser`.

--- src/services/auth/get-or-create-user.ts
+++ src/services/auth/get-or-create-user.ts
@@ -3,32 +3,46 @@ import { PrismaClient, User } from '@prisma/client'
 
 import getLinearUser from '@aces/services/auth/linear-user'
 import decrypt from '@aces/util/encryption/decrypt'
-import encrypt from '@aces/util/encryption/encrypt'
 
 
 const prisma = new PrismaClient()
 
-async function getOrCreateUser(accessToken: string, encrypted: boolean = false): Promise<User> {
-  if (encrypted) {
-    accessToken = decrypt(accessToken)
-  }
-  const linearUser: LinearUser = await getLinearUser(accessToken)
-  const encryptedAccessToken = encrypt(accessToken)
+
+async function getAndUpdateLinearUser(accessToken: string): Promise<User> {
+  const linearToken = decrypt(accessToken)
+  const linearUser: LinearUser = await getLinearUser(linearToken)
   const user = await prisma.user.upsert({
     where: { linearId: linearUser.id },
     update: {
       email: linearUser.email,
       displayName: linearUser.displayName,
-      token: encryptedAccessToken,
+      token: accessToken,
     },
     create: {
       email: linearUser.email,
       displayName: linearUser.displayName,
       linearId: linearUser.id,
-      token: encryptedAccessToken,
+      token: accessToken,
     },
   })
   return user
+}
+
+async function getOrCreateUser(accessToken: string, encrypted: boolean = false): Promise<User> {
+  let decryptedAccessToken = accessToken
+  if (encrypted) {
+    decryptedAccessToken = decrypt(accessToken)
+  }
+  if (decryptedAccessToken.startsWith('anonymous-')) {
+    return prisma.user.findUniqueOrThrow({
+      where: {
+        token: accessToken,
+      }
+    })
+  }
+  else {
+    return getAndUpdateLinearUser(accessToken)
+  }
 }
 
 export default getOrCreateUser

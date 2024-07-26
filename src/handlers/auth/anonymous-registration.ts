57b1543375dfe8bbf8c7bb0bbfe6b2d580bb1be6
Jens Astrup <jensaiden@gmail.com>
2024-07-25 22:54:01 -0400

    Add anonymous users to round
    On anonymous registration, add the user to specified round

--- src/handlers/auth/anonymous-registration.ts
+++ src/handlers/auth/anonymous-registration.ts
@@ -8,12 +8,14 @@ import encrypt from '@aces/util/encryption/encrypt'
 
 const prisma = new PrismaClient()
 
-interface AnonymousRegistrationData {
-  roundId: string
+
+interface AnonymousRegistrationRequest extends Request {
+  body: {
+    roundId: string
+  }
 }
 
-async function anonymousRegistration(request: Request, response: Response): Promise<void> {
-  console.log('Anonymous registration')
+async function anonymousRegistration(request: AnonymousRegistrationRequest, response: Response): Promise<void> {
   const randomId = uuidv7()
   const token = `anonymous-${randomId}`
   const encryptedToken = encrypt(token)
@@ -22,7 +24,20 @@ async function anonymousRegistration(request: Request, response: Response): Prom
       token: encryptedToken,
     }
   })
-  response.status(HttpStatusCodes.CREATED).json({ token, user })
+  // Add user as guest to round
+  await prisma.round.update({
+    where: {
+      id: request.body.roundId
+    },
+    data: {
+      guests: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+  response.status(HttpStatusCodes.CREATED).json({ user })
 }
 
 export default anonymousRegistration

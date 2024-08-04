1aff4a15b4ab2ff162b4b28ec7dba1629d6f5ac6
Jens Astrup <jensaiden@gmail.com>
2024-08-04 13:44:36 -0400

    Add coverage

--- tests/services/rounds/round-notifier.test.ts
+++ tests/services/rounds/round-notifier.test.ts
@@ -8,7 +8,10 @@ import sendMessageToRound from '@aces/socket/send-message-to-round'
 jest.mock('@prisma/client', () => {
   const mockPrismaClient = {
     vote: {
-      findMany: jest.fn()
+      findMany: jest.fn(),
+    },
+    round: {
+      findUnique: jest.fn()
     }
   }
   return {
@@ -22,7 +25,7 @@ describe('RoundNotifier', () => {
   let roundNotifier: RoundNotifier
   let mockRound: Round
   let mockIssue: Issue
-  let mockPrismaClient: { vote: { findMany: jest.Mock } }
+  let mockPrismaClient: { vote: { findMany: jest.Mock }, round: { findUnique: jest.Mock } }
   let mockSendMessageToRound: jest.Mock
 
   beforeEach(() => {
@@ -30,10 +33,30 @@ describe('RoundNotifier', () => {
     mockIssue = { id: 'issue-1' } as Issue
     // @ts-expect-error mock implementation
     roundNotifier = new RoundNotifier(mockRound)
-    mockPrismaClient = new PrismaClient() as unknown as { vote: { findMany: jest.Mock } }
+    mockPrismaClient = new PrismaClient() as unknown as { vote: { findMany: jest.Mock }, round: { findUnique: jest.Mock } }
     mockSendMessageToRound = sendMessageToRound as jest.Mock
 
     jest.clearAllMocks()
+  })
+
+  it('should return round with relations when get is called', async () => {
+    const mockRoundWithRelations = { ...mockRound, guests: [], issues: [] } as Round
+
+    mockPrismaClient.round.findUnique.mockResolvedValue(mockRoundWithRelations)
+
+    const round = await RoundNotifier.get(mockRound.id)
+
+    expect(mockPrismaClient.round.findUnique).toHaveBeenCalledWith({
+      where: {
+        id: mockRound.id
+      },
+      include: {
+        guests: true,
+        issues: true
+      }
+    })
+
+    expect(round).toEqual(mockRoundWithRelations)
   })
 
   it('should fetch votes, map them correctly, and send a message when voteSet is called', async () => {

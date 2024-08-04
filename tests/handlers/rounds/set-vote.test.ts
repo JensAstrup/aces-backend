56542e899c03d6da75828f0e5c47f65511af36d0
Jens Astrup <jensaiden@gmail.com>
2024-08-04 13:28:44 -0400

    Update tests

--- tests/handlers/rounds/set-vote.test.ts
+++ tests/handlers/rounds/set-vote.test.ts
@@ -1,75 +1,77 @@
-import { PrismaClient } from '@prisma/client'
+import { jest } from '@jest/globals'
+import { Issue, Round, RoundGuest, User } from '@prisma/client'
 import { Request, Response } from 'express'
 
 import HttpStatusCodes from '@aces/common/HttpStatusCodes'
 import setVoteHandler from '@aces/handlers/rounds/set-vote'
-import getIssue from '@aces/services/issues/get-issue'
 import RoundNotifier from '@aces/services/rounds/round-notifier'
 import setVote from '@aces/services/rounds/set-vote'
 import canAccessRound from '@aces/util/can-access-round'
 
 
-jest.mock('@prisma/client', () => {
-  const mockPrismaClient = {
-    round: {
-      findUnique: jest.fn()
-    }
-  }
-  return {
-    PrismaClient: jest.fn(() => mockPrismaClient)
-  }
-})
+jest.mock('@aces/services/rounds/round-notifier')
 jest.mock('@aces/services/rounds/set-vote')
 jest.mock('@aces/util/can-access-round')
-jest.mock('@aces/services/issues/get-issue')
-jest.mock('@aces/services/rounds/round-notifier')
 
 describe('setVoteHandler', () => {
   let mockRequest: Partial<Request>
   let mockResponse: Partial<Response>
-  let mockUser: { id: string }
-  let mockPrismaClient: { round: { findUnique: jest.Mock } }
-  let mockRoundNotifier: jest.Mocked<RoundNotifier>
-  let mockCanAccessRound: jest.Mock
-  let mockGetIssue: jest.Mock
-  let mockSetVote: jest.Mock
+  let mockUser: User
+  let mockCanAccessRound: jest.MockedFunction<typeof canAccessRound>
+  let mockSetVote: jest.MockedFunction<typeof setVote>
+  let MockedRoundNotifier: jest.Mocked<typeof RoundNotifier>
 
   beforeEach(() => {
-    mockUser = { id: 'user-1' }
+    mockUser = {
+      id: 'user-1',
+      linearId: null,
+      email: 'user@example.com',
+      displayName: 'Test User',
+      token: 'user-token',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
     mockRequest = {
       user: mockUser,
       params: { roundId: 'round-1' },
-      body: { issueId: 'issue-1', vote: 5 }
-    } as unknown as Request
+      body: { issueId: 'linear-1', vote: 5 }
+    }
     mockResponse = {
+    // @ts-expect-error mock implementation
       status: jest.fn().mockReturnThis(),
+      // @ts-expect-error mock implementation
       json: jest.fn()
-    } as unknown as Response
-    mockPrismaClient = new PrismaClient() as unknown as { round: { findUnique: jest.Mock } }
-    mockRoundNotifier = {
-      voteSet: jest.fn()
-    } as unknown as jest.Mocked<RoundNotifier>
-    mockCanAccessRound = canAccessRound as jest.Mock
-    mockGetIssue = getIssue as jest.Mock
-    mockSetVote = setVote as jest.Mock
-    (RoundNotifier as jest.Mock).mockImplementation(() => mockRoundNotifier)
+    }
+    mockCanAccessRound = canAccessRound as jest.MockedFunction<typeof canAccessRound>
+    mockSetVote = setVote as jest.MockedFunction<typeof setVote>
+    MockedRoundNotifier = RoundNotifier as jest.Mocked<typeof RoundNotifier>
+
+    // Reset mocks
     jest.clearAllMocks()
   })
 
   it('should set vote and return OK when user is authorized and round and issue exist', async () => {
-    const mockRound = { id: 'round-1' }
-    const mockIssue = { id: 'issue-1', roundId: 'round-1', linearId: 'issue-1' }
+    const mockRound = {
+      id: 'round-1',
+      creatorId: 'creator-1',
+      status: 'CREATED',
+      currentIssueId: 'issue-1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      guests: [{ userId: 'user-2', roundId: 'round-1' }],
+      issues: [{ id: 'issue-1', roundId: 'round-1', linearId: 'linear-1' }]
+    } as { guests: RoundGuest[], issues: Issue[] } & Round
+
     mockCanAccessRound.mockResolvedValue(true)
-    mockPrismaClient.round.findUnique.mockResolvedValue(mockRound)
-    mockGetIssue.mockResolvedValue(mockIssue)
+    MockedRoundNotifier.get.mockResolvedValue(mockRound)
+    MockedRoundNotifier.prototype.voteSet = jest.fn()
 
     await setVoteHandler(mockRequest as Request, mockResponse as Response)
 
     expect(mockCanAccessRound).toHaveBeenCalledWith('round-1', mockUser)
-    expect(mockPrismaClient.round.findUnique).toHaveBeenCalledWith({ where: { id: 'round-1' } })
-    expect(mockSetVote).toHaveBeenCalledWith('round-1', 'issue-1', 5, mockUser)
-    expect(mockGetIssue).toHaveBeenCalledWith({ roundId: 'round-1', linearId: 'issue-1' })
-    expect(mockRoundNotifier.voteSet).toHaveBeenCalledWith(mockIssue)
+    expect(mockSetVote).toHaveBeenCalledWith('round-1', 'linear-1', 5, mockUser)
+    expect(MockedRoundNotifier.get).toHaveBeenCalledWith('round-1')
+    expect(MockedRoundNotifier.prototype.voteSet).toHaveBeenCalledWith(mockRound.issues[0])
     expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCodes.OK)
     expect(mockResponse.json).toHaveBeenCalledWith({ message: 'Estimate set' })
   })
@@ -80,7 +82,7 @@ describe('setVoteHandler', () => {
     await setVoteHandler(mockRequest as Request, mockResponse as Response)
 
     expect(mockCanAccessRound).not.toHaveBeenCalled()
-    expect(mockPrismaClient.round.findUnique).not.toHaveBeenCalled()
+    expect(MockedRoundNotifier.get).not.toHaveBeenCalled()
     expect(mockSetVote).not.toHaveBeenCalled()
     expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCodes.UNAUTHORIZED)
     expect(mockResponse.json).toHaveBeenCalledWith({ error: 'Unauthorized' })
@@ -92,7 +94,7 @@ describe('setVoteHandler', () => {
     await setVoteHandler(mockRequest as Request, mockResponse as Response)
 
     expect(mockCanAccessRound).toHaveBeenCalledWith('round-1', mockUser)
-    expect(mockPrismaClient.round.findUnique).not.toHaveBeenCalled()
+    expect(MockedRoundNotifier.get).not.toHaveBeenCalled()
     expect(mockSetVote).not.toHaveBeenCalled()
     expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCodes.FORBIDDEN)
     expect(mockResponse.json).toHaveBeenCalledWith({ error: 'Forbidden' })
@@ -100,29 +102,39 @@ describe('setVoteHandler', () => {
 
   it('should return NOT_FOUND when round does not exist', async () => {
     mockCanAccessRound.mockResolvedValue(true)
-    mockPrismaClient.round.findUnique.mockResolvedValue(null)
+    MockedRoundNotifier.get.mockResolvedValue(null)
 
     await setVoteHandler(mockRequest as Request, mockResponse as Response)
 
     expect(mockCanAccessRound).toHaveBeenCalledWith('round-1', mockUser)
-    expect(mockPrismaClient.round.findUnique).toHaveBeenCalledWith({ where: { id: 'round-1' } })
+    expect(MockedRoundNotifier.get).toHaveBeenCalledWith('round-1')
     expect(mockSetVote).not.toHaveBeenCalled()
     expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCodes.NOT_FOUND)
     expect(mockResponse.json).toHaveBeenCalledWith({ error: 'Round not found' })
   })
 
   it('should return NOT_FOUND when issue does not exist', async () => {
-    const mockRound = { id: 'round-1' }
+    const mockRound = {
+      id: 'round-1',
+      creatorId: 'creator-1',
+      status: 'CREATED',
+      currentIssueId: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      guests: [{ userId: 'user-2', roundId: 'round-1' }],
+      issues: []
+    } as { guests: RoundGuest[], issues: Issue[] } & Round
+
     mockCanAccessRound.mockResolvedValue(true)
-    mockPrismaClient.round.findUnique.mockResolvedValue(mockRound)
-    mockGetIssue.mockResolvedValue(null)
+    MockedRoundNotifier.get.mockResolvedValue(mockRound)
+    MockedRoundNotifier.prototype.voteSet = jest.fn()
 
     await setVoteHandler(mockRequest as Request, mockResponse as Response)
 
     expect(mockCanAccessRound).toHaveBeenCalledWith('round-1', mockUser)
-    expect(mockPrismaClient.round.findUnique).toHaveBeenCalledWith({ where: { id: 'round-1' } })
-    expect(mockSetVote).toHaveBeenCalledWith('round-1', 'issue-1', 5, mockUser)
-    expect(mockGetIssue).toHaveBeenCalledWith({ roundId: 'round-1', linearId: 'issue-1' })
+    expect(mockSetVote).toHaveBeenCalledWith('round-1', 'linear-1', 5, mockUser)
+    expect(MockedRoundNotifier.get).toHaveBeenCalledWith('round-1')
+    expect(MockedRoundNotifier.prototype.voteSet).not.toHaveBeenCalled()
     expect(mockResponse.status).toHaveBeenCalledWith(HttpStatusCodes.NOT_FOUND)
     expect(mockResponse.json).toHaveBeenCalledWith({ error: 'Issue not found' })
   })
